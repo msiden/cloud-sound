e8e606006341a641d68ded37599df4a2a12fa609
Martin Sidén <martin.siden@live.se>
2024-02-25 18:13:23 +0100

    wip

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-document.querySelector('form').addEventListener('submit', (e) => {
+/* document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   fetch('http://localhost:3001/upload', {
@@ -9,9 +9,9 @@ document.querySelector('form').addEventListener('submit', (e) => {
   }).catch((error) => {
     console.error(error);
   });
-});
+}); */
 
-fetch('http://localhost:3001/songs').then((response) => {
+/* fetch('http://localhost:3001/songs').then((response) => {
   response.json().then((songs) => {
     songs.forEach((song) => {
       const songElement = document.createElement('audio');
@@ -22,4 +22,22 @@ fetch('http://localhost:3001/songs').then((response) => {
   });
 }).catch((error) => {
   console.error(error);
-});
+}); */
+
+
+function getUser() {
+  fetch('http://user_api:8080/user/{user_id}').then((response) => {
+    response.json().then((users) => {
+      console.log('>>>',users)
+    });
+  }).catch((error) => {
+    console.error(error);
+  }); 
+}
+
+function helloWorld() {
+  console.log('hello world')
+  getUser()
+}
+
+window.helloWorld = helloWorld
