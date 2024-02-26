e8636ecdd26eadfb3706dc8a5467586fd62b424a
Martin Sidén <martin.siden@bambora.com>
2024-02-26 14:31:51 +0100

    api service done

---
+++ cloud-sound2/db.py
@@ -0,0 +1,89 @@
+from sqlalchemy import String, Integer, Float, and_
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.exc import OperationalError
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+import uuid
+import datetime
+
+
+
+engine = create_engine("sqlite:///database.db")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Files(Base):
+    __tablename__ = "files"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    created_at: Mapped[str] = mapped_column(String)
+    created_by: Mapped[int] = mapped_column(String)
+
+
+class FilesSchema(SQLAlchemySchema):
+    class Meta:
+        model = Files
+        load_instance = True
+
+    id = auto_field()
+    name = auto_field()
+    created_at = auto_field()
+    created_by = auto_field()
+
+
+def create_all():
+    try:
+        Base.metadata.create_all(engine)
+    except OperationalError:
+        print("DB connection failed")
+
+
+def insert_data(file_id: str, name: str, user_id: int) -> uuid.uuid4:
+    with Session(engine) as session:
+
+        created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        new_entry = Files(
+            id=file_id,
+            name=name,
+            created_at=created_at,
+            created_by=user_id
+        )
+
+        session.add(new_entry)
+        session.commit()
+
+    return id
+
+
+def get_data() -> dict:
+    files_schema = FilesSchema(many=True)
+
+    with Session(engine) as session:
+        statement = select(Files).order_by(Files.created_at.desc()) #.limit(chunk_size)
+        result = session.scalars(statement).all()
+        return files_schema.dump(result)
+
+
+def delete(file_id: str, user_id: int) -> int:
+    with Session(engine) as session:
+        query = session.query(Files).filter(and_((Files.id==file_id), (Files.created_by==user_id)))
+        count = query.count()
+        query.delete()
+        session.commit()
+    return count
+
+
+create_all()
+
+# insert_data("file1.mp3", 1)
+# insert_data("file2.mp3", 1)
+# insert_data("test.wav", 2)
+# insert_data("file1.mp3", 3)
---
+++ cloud-sound2/endpoints.py
@@ -0,0 +1,35 @@
+import requests
+from flask import request
+import uuid
+import db
+
+
+def sounds() -> dict:
+    return db.get_data()
+
+
+def new_user(user_id: int) -> int:
+    response = requests.post('http://user_api:8000/user', json={"id": user_id})
+    return response.status_code
+
+
+def upload() -> int:
+    user_id = request.form["user_id"]
+    file = request.files["file"]
+
+    response = requests.get(f'http://user_api:8000/user/{user_id}')
+
+    if response.status_code == 200:
+        file_id = str(uuid.uuid4())
+        db.insert_data(file_id, file.filename, user_id)
+        requests.post('http://file_store:3000/files/', files={file_id: file})
+
+    return response.status_code
+
+
+def delete(user_id: int, file_id: str) -> int:
+    count = db.delete(file_id, user_id)
+    if count > 0:
+        response = requests.delete(f'http://file_store:3000/files/{file_id}')
+        return response.status_code
+    return 404
