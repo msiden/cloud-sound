f0f82a577dc96a9e56b418a5e67c80f18806b81c
Martin Sidén <martin.siden@bambora.com>
2024-02-26 17:01:35 +0100

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,43 +1,80 @@
-/* document.querySelector('form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  fetch('http://localhost:3001/upload', {
-    method: 'POST',
-    body: formData,
-  }).then(() => {
-    window.location.reload();
-  }).catch((error) => {
-    console.error(error);
-  });
-}); */
+// document.querySelector('uploadForm').addEventListener('submit', (e) => {
+//   e.preventDefault();
 
-/* fetch('http://localhost:3001/songs').then((response) => {
+//   const formData = new FormData(e.target);
+//   fetch('http://localhost:8080/upload', {
+//     method: 'POST',
+//     body: formData,
+//   }).then(() => {
+//     // window.location.reload();
+//     console.log('>>')
+//     alert('hej')
+//   }).catch((error) => {
+//     console.error(error);
+//   });
+// });
+
+fetch('http://localhost:8080/sounds').then((response) => {
   response.json().then((songs) => {
     songs.forEach((song) => {
-      const songElement = document.createElement('audio');
-      songElement.controls = 'controls';
-      songElement.src = song.url;
-      document.querySelector('.songs').appendChild(songElement);
+      const parentDiv = document.createElement('div');
+
+      const nameDiv = document.createElement('div');
+      const name = document.createTextNode(`Name: ${song.name}`);
+      nameDiv.appendChild(name);
+      parentDiv.appendChild(nameDiv)
+
+      const dateDiv = document.createElement('div');
+      const date = document.createTextNode(`Date: ${song.created_at}`);
+      dateDiv.appendChild(date);
+      parentDiv.appendChild(dateDiv)
+
+
+      const userDiv = document.createElement('div');
+      const user = document.createTextNode(`User: ${song.created_by}`);
+      userDiv.appendChild(user);
+      parentDiv.appendChild(userDiv)
+
+      const idDiv = document.createElement('div');
+      const id = document.createTextNode(`Id: ${song.id}`);
+      idDiv.appendChild(id);
+      parentDiv.appendChild(idDiv)
+
+      document.querySelector('.songs').appendChild(parentDiv);
     });
   });
 }).catch((error) => {
   console.error(error);
-}); */
+});
 
+function createUser(event) {
+  event.preventDefault();
+  var newUserId = document.getElementById("userId").value;
 
-function getUser() {
-  fetch('http://user_api:8080/user/{user_id}').then((response) => {
-    response.json().then((users) => {
-      console.log('>>>',users)
-    });
+  fetch(`http://localhost:8080/new_user?user_id=${newUserId}`, {method: 'POST'}).then((response) => {
+    let content
+    if (response.status === 200) {
+      content = document.createTextNode('User created');
+    } else {
+      content = document.createTextNode('User already exists');
+    }
+
+    const nameDiv = document.createElement('div');
+    nameDiv.appendChild(content);
+    document.querySelector('.user').appendChild(nameDiv);
   }).catch((error) => {
-    console.error(error);
-  }); 
+    console.error('>>>>', error);
+  });
 }
 
-function helloWorld() {
-  console.log('hello world')
-  getUser()
+function uploadFile(event) {
+  event.preventDefault();
+  console.log('>>>>')
 }
 
-window.helloWorld = helloWorld
+let userForm = document.getElementById("newUserId");
+userForm.addEventListener('submit', createUser);
+
+// let uploadForm = document.getElementById("upload");
+// uploadForm.addEventListener('submit', uploadFile);
+// window.createUser = createUser
